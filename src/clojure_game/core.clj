12ac3a67c8e99733809320b80ecfbfaf709d8c5e
divya2661 <divn2661@gmail.com>
2018-07-04 17:44:09 +0530

    Can push and read messages from server

--- src/clojure_game/core.clj
+++ src/clojure_game/core.clj
@@ -1,7 +1,25 @@
 (ns clojure-game.core
+  (:import java.net.ServerSocket)
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn read-message
+  "read messages from a given client socket"
+  [client-socket]
+  (let [reader (clojure.java.io/reader client-socket)]
+    (.readLine reader)))
+
+(defn write-message [client-socket message]
+  "write messages into a given client socket"
+  (let [writer (clojure.java.io/writer client-socket)]
+    (.write writer message)
+    (.flush writer)))
+
+(defn -main [& args]
+  (let [port 1234]
+    (with-open [server-socket (ServerSocket. port)
+                client-socket (do (prn "started the server")
+                                  (.accept server-socket))]
+      (let [message (read-message client-socket)]
+        (prn message)
+        (write-message client-socket (str " from server " message))))))
